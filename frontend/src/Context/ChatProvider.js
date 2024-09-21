2c03215108f7a901c16a6c53e1b6469b075411b3
GopaltiwariIET <gopaltiwari28122003@gmail.com>
2024-09-21 16:29:00 +0530

    build frontend

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,44 +1,43 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  const [selectedChat, setSelectedChat] = useState();
-  const [user, setUser] = useState();
-  const [notification, setNotification] = useState([]);
-  const [chats, setChats] = useState();
+	const [selectedChat, setSelectedChat] = useState();
+	const [user, setUser] = useState();
+	const [notification, setNotification] = useState([]);
+	const [chats, setChats] = useState();
 
-  const history = useHistory();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-    setUser(userInfo);
+	useEffect(() => {
+		const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+		setUser(userInfo);
 
-    if (!userInfo) history.push("/");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [history]);
+		if (!userInfo) navigate("/");
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [navigate]);
 
-  return (
-    <ChatContext.Provider
-      value={{
-        selectedChat,
-        setSelectedChat,
-        user,
-        setUser,
-        notification,
-        setNotification,
-        chats,
-        setChats,
-      }}
-    >
-      {children}
-    </ChatContext.Provider>
-  );
+	return (
+		<ChatContext.Provider
+			value={{
+				selectedChat,
+				setSelectedChat,
+				user,
+				setUser,
+				notification,
+				setNotification,
+				chats,
+				setChats,
+			}}>
+			{children}
+		</ChatContext.Provider>
+	);
 };
 
 export const ChatState = () => {
-  return useContext(ChatContext);
+	return useContext(ChatContext);
 };
 
 export default ChatProvider;
